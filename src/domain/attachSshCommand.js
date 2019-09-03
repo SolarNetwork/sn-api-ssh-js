2a506c21c48804deb3fa72c02021ee2b09e4db45
Matt Magoffin <matt@solarnetwork.net>
2019-09-04 07:11:36 +1200

    Reformat with prettier.

--- src/domain/attachSshCommand.js
+++ src/domain/attachSshCommand.js
@@ -1,40 +1,49 @@
-import SshCommand from './sshCommand';
-import SshTerminalSettings from './sshTerminalSettings';
+import SshCommand from "./sshCommand";
+import SshTerminalSettings from "./sshTerminalSettings";
 
 /** The command for attaching to a SSH terminal shell. */
-export const SolarSshCommandAttachSsh = 'attach-ssh';
+export const SolarSshCommandAttachSsh = "attach-ssh";
 
 /**
  * SSH command for attaching to a SSH terminal shell.
  */
 class AttachSshCommand extends SshCommand {
-    /**
-     * Constructor.
-     * 
-     * @param {string} authorization a pre-computed SNWS2 authorization header, which must match
-     *        exactly a `GET` request to the `/solaruser/api/v1/sec/nodes/meta/:nodeId`
-     *        path using the provided authorization date and, node ID.
-     * @param {Date|number} authorizationDate the date used in the `authorization` value
-     * @param {string} username the SSH username to use
-     * @param {string} password the SSH password to use
-     * @param {SshTerminalSettings} [terminalSettings] optional terminal settings to use
-     */
-    constructor(authorization, authorizationDate, username, password, terminalSettings) {
-        const data = {};
-        data['authorization'] = authorization;
-        data['authorization-date'] = (authorizationDate instanceof Date ? authorizationDate.getTime() : authorizationDate);
-        data['username'] = username;
-        data['password'] = password;
-        if ( terminalSettings instanceof SshTerminalSettings ) {
-            const termOpts = JSON.parse(terminalSettings.toJsonEncoding());
-            for ( const prop of Object.keys(termOpts) ) {
-                if ( data[prop] === undefined ) {
-                    data[prop] = termOpts[prop];
-                }
-            }
+  /**
+   * Constructor.
+   *
+   * @param {string} authorization a pre-computed SNWS2 authorization header, which must match
+   *        exactly a `GET` request to the `/solaruser/api/v1/sec/nodes/meta/:nodeId`
+   *        path using the provided authorization date and, node ID.
+   * @param {Date|number} authorizationDate the date used in the `authorization` value
+   * @param {string} username the SSH username to use
+   * @param {string} password the SSH password to use
+   * @param {SshTerminalSettings} [terminalSettings] optional terminal settings to use
+   */
+  constructor(
+    authorization,
+    authorizationDate,
+    username,
+    password,
+    terminalSettings
+  ) {
+    const data = {};
+    data["authorization"] = authorization;
+    data["authorization-date"] =
+      authorizationDate instanceof Date
+        ? authorizationDate.getTime()
+        : authorizationDate;
+    data["username"] = username;
+    data["password"] = password;
+    if (terminalSettings instanceof SshTerminalSettings) {
+      const termOpts = JSON.parse(terminalSettings.toJsonEncoding());
+      for (const prop of Object.keys(termOpts)) {
+        if (data[prop] === undefined) {
+          data[prop] = termOpts[prop];
         }
-        super(SolarSshCommandAttachSsh, data);
+      }
     }
+    super(SolarSshCommandAttachSsh, data);
+  }
 }
 
 export default AttachSshCommand;
--- src/domain/sshCloseCode.js
+++ src/domain/sshCloseCode.js
@@ -1,43 +1,41 @@
 /** @module domain */
 
-import { ComparableEnum } from 'solarnetwork-api-core';
+import { ComparableEnum } from "solarnetwork-api-core";
 
 /**
  * A named socket close code.
  */
 class SshCloseCode extends ComparableEnum {
-
-    /**
-     * Constructor.
-     * 
-     * @param {string} name the name
-     * @param {value} value the value
-     */
-    constructor(name, value) {
-        super(name, value);
-        if ( this.constructor === SshCloseCode ) {
-            Object.freeze(this);
-        }
+  /**
+   * Constructor.
+   *
+   * @param {string} name the name
+   * @param {value} value the value
+   */
+  constructor(name, value) {
+    super(name, value);
+    if (this.constructor === SshCloseCode) {
+      Object.freeze(this);
     }
+  }
 
-    /**
-	 * Get the {@link SshCloseCodes} values.
-	 * 
-	 * @inheritdoc
-	 */
-	static enumValues() {
-		return SshCloseCodeValues;
-	}
-
+  /**
+   * Get the {@link SshCloseCodes} values.
+   *
+   * @inheritdoc
+   */
+  static enumValues() {
+    return SshCloseCodeValues;
+  }
 }
 
 const SshCloseCodeValues = Object.freeze([
-	new SshCloseCode('AUTHENTICATION_FAILURE', 4000),
+  new SshCloseCode("AUTHENTICATION_FAILURE", 4000)
 ]);
 
 /**
  * The enumeration of supported SshCloseCode values.
- * 
+ *
  * @readonly
  * @enum {SshCloseCode}
  * @property {SshCloseCode} AUTHENTICATION_FAILURE an authentication failure
