8d8009d1c92da8331c4dcfbf7d4ebfff8f80f60c
Matt Magoffin <matt@solarnetwork.net>
2025-05-17 10:09:48 +1200

    Remove code; add link to sn-api-core-js.

--- lib.js
+++
@@ -1,2 +0,0 @@
-export * from './src/domain/domain';
-export * from './src/net/net';
--- src/domain/domain.js
+++
@@ -1,5 +0,0 @@
-export { default as AttachSshCommand } from "./attachSshCommand";
-export { default as SshCloseCodes, SshCloseCode } from "./sshCloseCode";
-export { default as SshCommand } from "./sshCommand";
-export { default as SshSession } from "./sshSession";
-export { default as SshTerminalSettings } from "./sshTerminalSettings";
